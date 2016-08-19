ac0c30cc6f95ece4f85609eec9b55320233ce61e
saulgar <saul.alejandro.garcia.garcia@gmail.com>
2016-08-19 08:21:18 -0500

    changes in ciclos.py

--- ciclos.py
+++ ciclos.py
@@ -1,2 +1,14 @@
-for c in range(5,10,2):
-	print (c)
+#for c in range(5,10,2):
+#	print (c)
+
+alfabeto ="abcdefghijklmnopqrstuvwxyz"
+contrasenia = "fa"
+for l1 in alfabeto:
+	for l2 in alfabeto:
+		ca = l1+l2
+		print (ca)
+		if ca == contrasenia:
+			print ("contrasenia invalida")
+
+
+	
